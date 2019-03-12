2c4df484b24a8c87fe0703f48a8c9b184c5655cd
Jeff <jeff@snyk.io>
2019-03-12 15:49:33 -0400

    feat: added command line arguments fix: adapt to modified snyk_dependencies_list_all_dependencies_by_project function which has been fixed to do pagination

--- api-demo-3-list-dependencies.py
+++ api-demo-3-list-dependencies.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 import SnykAPI
 
@@ -7,8 +8,30 @@ def print_json(json_obj):
     print(json.dumps(json_obj, indent=4))
 
 
-org_id = ''  # TODO: put in your org_id
-project_id = ''  # TODO: put in your project_id
+def parse_command_line_args():
+    parser = argparse.ArgumentParser(description="Snyk API Examples")
+    parser.add_argument('--orgId', type=str,
+                        help='The Snyk Organisation Id')
+
+    parser.add_argument('--projectId', type=str,
+                        help='The project ID in Snyk')
+
+    args = parser.parse_args()
+
+    if args.orgId is None:
+        parser.error('You must specify --orgId')
+
+    if args.projectId is None:
+        parser.error('You must specify --projectId')
+
+    return args
+
+
+# TODO: specify --orgId=<your-org-id> as a command line parameter or just manually set it here in the code
+# TODO: specify --projectId=<your-org-id> as a command line parameter or just manually set it here in the code
+args = parse_command_line_args()
+org_id = args.orgId
+project_id = args.projectId
 
 
 request_payload = {
@@ -17,12 +40,10 @@ request_payload = {
     ]
 }
 
-
 # List issues in a project
-json_res = SnykAPI.snyk_dependencies_list_all_dependencies_by_project(org_id, project_id)
+lst_licenses = SnykAPI.snyk_dependencies_list_all_dependencies_by_project(org_id, project_id)
 
-print(json_res)
-for v in json_res['results']:
+for v in lst_licenses:
     print('\n%s: %s@%s' % (v['type'], v['name'], v['version']))
 
     licenses = v['licenses']
