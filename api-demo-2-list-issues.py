4c1cc54488abfab2dfaf88b8a29150b8de31cb2a
Jeff <jeff@snyk.io>
2019-03-13 23:06:14 -0400

    chore: added arg parsing to demo 2

--- api-demo-2-list-issues.py
+++ api-demo-2-list-issues.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 import SnykAPI
 
@@ -7,8 +8,30 @@ def print_json(json_obj):
     print(json.dumps(json_obj, indent=4))
 
 
-org_id = ''  # TODO: put in your org_id
-project_id = ''  # TODO: put in your project_id
+def parse_command_line_args():
+    parser = argparse.ArgumentParser(description="Snyk API Examples")
+    parser.add_argument('--orgId', type=str,
+                        help='The Snyk Organisation Id')
+
+    parser.add_argument('--projectId', type=str,
+                        help='The project ID in Snyk')
+
+    args = parser.parse_args()
+
+    if args.orgId is None:
+        parser.error('You must specify --orgId')
+
+    if args.projectId is None:
+        parser.error('You must specify --projectId')
+
+    return args
+
+
+# TODO: specify --orgId=<your-org-id> as a command line parameter or just manually set it here in the code
+# TODO: specify --projectId=<your-org-id> as a command line parameter or just manually set it here in the code
+args = parse_command_line_args()
+org_id = args.orgId
+project_id = args.projectId
 
 
 # List issues in a project
