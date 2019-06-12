535fb0ccd465506f57b11f756ac9dd5a8364bfa6
Shawn <shawn@snyk.io>
2019-06-11 23:27:41 -0400

    Refactor

--- api-demo-2b-issues-analytics.py
+++ api-demo-2b-issues-analytics.py
@@ -4,167 +4,180 @@ import SnykAPI
 import operator
 from collections import OrderedDict
 ###SAMPLE RUN COMMAND: python3 api-demo-2b-issues-analytics.py
-## make sure to install pathlib, requests, json
-##Update org_id and my_js_goof_project_id
-
-#def print_json(json_obj):
-    #print(json.dumps(json_obj, indent=4))
-
-
 
 org_id = ''
 my_js_goof_project_id = ''
-
-
-# List issues in a project
-json_res = SnykAPI.snyk_projects_project_issues(org_id, my_js_goof_project_id)
+json_rslts_repo = ''
 #print(json.dumps(json_res,indent=2))
 
-def manageDict(dict, key): 
-    if keyExists(dict, key): 
-        dict[key] +=1 
-    else: 
-        dict[key] = 1
-    return dict
+###START CLASS
+class analysisLog:
+    iCritical=0
+    iHigh=1
+    iMedium=2
+    iLow=3
+    arrSevDescriptions=['Critical', 'High','Medium','Low']
+    #special convention for deps
+    iDepByCriticalSev=0
+    iDepByHighSev=1
+    iDepIssueCounts=2
+    sDepDescriptions=['By Number of Critical Severity Issues', 'By High Severity Issues', 'By Total Issue Count']
 
-def keyExists(dict, key):
-    if key in dict: 
-        return True 
-    else: 
-        return False
+    def __init__(self, analysisObjID,reponame):
+        #license and issue management
+        self.id = analysisObjID
+        self.reponame = reponame
+        self.vulnIssueCounts = [0,0,0,0]
+        self.licenseIssueCounts = [0,0,0,0]
+        self.dictVulnIssues = [{},{},{},{}]
+        self.dictLicenseIssues = [{},{},{},{}]
 
-def printDict(title, dict,isNumber):
-    if(title != ''):
-        print(title)
-    for key in dict: 
-        if(isNumber == True):
-            print('\t\tIssue: ' + key + ' , Count:' + str(dict[key]))
-        else:
-            print('\t\tIssue: ' + key + ' , Count:' + dict[key]) 
-    if(len(dict)==0):
-        print('\t\tNone found')
+        #special convention for deps
+        self.dictDepIssues = [{},{},{}] #dictDepCritical,dictDepHigh,dictDepIssueCount
 
-def printTopDict(title, dict,isNumber,limiter):
-    counter=0
-    if(title != ''):
-        print(title)
-    for key in dict: 
-        counter+=1
-        if(counter>limiter):
-            break
-        if(isNumber == True):
-            print('\t\tIssue: ' + key + ' , Count:' + str(dict[key]))
-        else:
-            print('\t\tIssue: ' + key + ' , Count:' + dict[key]) 
-    if(len(dict)==0):
-        print('\t\tNone found')
+        ###DICTIONAIRY OF NEWS WORTHY ATTACKS####
+        #Searchlist
+        self.cveWatchlist = {}
+        #Description for printing - (ToDo: Read from file later)
+        self.cveWatchlist['CVE-2018-1000851'] = 'Copay Bitcoing attack via Event-Stream'
+        self.cveWatchlist['CVE-2017-5638'] = 'Equifax vulnerability via Struts'
+        self.cveWatchlist['CVE-2018-1002204'] = 'Zip Slip - Found by Snyk!'
+        
+        ########################################
+        self.cveWatchlistFound={}
 
-def sortDict(dict):
-    #sDict = sorted(dict.items(), key=operator.itemgetter(1))
-    sDictOrdered = OrderedDict(sorted(dict.items(), key=lambda x: x[1],reverse=True))
-    return sDictOrdered
-########
-# ANALYTIC GOAL 1
-# 1) MOST PREVALENT ISSUES by severity
-# 2) ISSUE COUNTS
-# 3) Criticals (CVSS 9.0 and above)
-#ANALYTIC GOAL 2
-# 1) Look for issues recently in news, i.e. event-stream to flag relevant issues
-#########
-countVulnCritical=0
-countVulnHigh=0
-countVulnMedium=0
-countVulnLow=0
+    #Utilities
+    def manageDict(self,dict, key): 
+        if self.keyExists(dict, key): 
+            dict[key] +=1 
+        else: 
+            dict[key] = 1
+        return dict
 
-countLicenseHigh=0
-countLicenseMedium=0
-countLicenseLow=0
+    def keyExists(self,dict, key):
+        if key in dict: 
+            return True 
+        else: 
+            return False
 
-dictDepCritical={}
-dictDepHigh={}
-dictDepIssueCount={}
+    def printDict(self,title, dict,isNumber):
+        if(title != ''):
+            print(title)
+        for key in dict: 
+            if(isNumber == True):
+                print('\t\tIssue: ' + key + ' , Count:' + str(dict[key]))
+            else:
+                print('\t\tIssue: ' + key + ' , Count:' + dict[key]) 
+        if(len(dict)==0):
+            print('\t\tNone found')
 
-dictVulnCritical={}
-dictVulnHigh={}
-dictVulnMedium={}
-dictVulnLow={}
+    def printDictWithDescriptDict(self,title, dictItems,dictDescription):
+        if(title != ''):
+            print(title)
+        for key in dictItems: 
+            print('\t\t' + key + ' , ' + dictDescription[key] + ', Count: ' + str(dictItems[key])) 
+        if(len(dictItems)==0):
+            print('\t\tNone found')
 
-dictLicenseHigh={}
-dictLicenseMedium={}
-dictLicenseLow={}
+    def printTopDict(self,title, dict,isNumber,limiter):
+        counter=0
+        if(title != ''):
+            print(title)
+        for key in dict: 
+            counter+=1
+            if(counter>limiter):
+                break
+            if(isNumber == True):
+                print('\t\tIssue: ' + key + ' , Count:' + str(dict[key]))
+            else:
+                print('\t\tIssue: ' + key + ' , Count:' + dict[key]) 
+        if(len(dict)==0):
+            print('\t\tNone found')
 
-cveWatchlist = {'CVE-2018-1000851'} #currently just eventstream
-cveWatchlistFound={}
-###Goal 1 - get counts
-for v in json_res['issues']['vulnerabilities']:
-    manageDict(dictDepIssueCount, v['package'] + ':' + v['version'])
-    if(v['severity']=='high'):
-        manageDict(dictVulnHigh, v['title'])
-        manageDict(dictDepHigh, v['package'] + ':' + v['version'])
-        countVulnHigh+=1
-    elif(v['severity']=='medium'):
-        manageDict(dictVulnMedium, v['title'])
-        countVulnMedium+=1
-    elif(v['severity']=='low'):
-        manageDict(dictVulnLow, v['title'])
-        countVulnLow+=1
-
-    if(v['cvssScore'] >= 9.0):
-        manageDict(dictVulnCritical, v['title'])
-        manageDict(dictDepCritical, v['package'] + ':' + v['version'])
-        countVulnCritical+=1
+    def sortDict(self,dict):
+        #sDict = sorted(dict.items(), key=operator.itemgetter(1))
+        sDictOrdered = OrderedDict(sorted(dict.items(), key=lambda x: x[1],reverse=True))
+        return sDictOrdered
     
-    curCVEList = v['identifiers']['CVE']
-    for curCVE in curCVEList:
-        if(keyExists(cveWatchlist,curCVE)):
-            #print('CVE Watchlist item found: ' + curCVE)
-            manageDict(cveWatchlistFound, curCVE)
+    ##OUTPUT FUNCTIONS
+    def printResultVulns(self):
+        print('\nVULNERABILITY ANALYSIS')
+        print('\n\tCritical Severity: ' + str(self.vulnIssueCounts[self.iCritical]) + ', High Severity: ' + str(self.vulnIssueCounts[self.iHigh]) + ', Medium Severity: ' + str(self.vulnIssueCounts[self.iMedium]) + ', Low Severity: ' + str(self.vulnIssueCounts[self.iLow]))
+        for iSev in range( 0 ,4) :            
+            self.printTopDict('\n\t' + self.arrSevDescriptions[iSev] + ' Severity Issues: ' , self.sortDict(self.dictVulnIssues[iSev]),True,5)
+        
+    def printResultLicense(self):
+        print('\nLICENSE ANALYSIS')
+        print('\n\tHigh Severity: ' + str(self.licenseIssueCounts[self.iHigh]) + ', Medium Severity: ' + str(self.licenseIssueCounts[self.iMedium]) + ', Low Severity: ' + str(self.licenseIssueCounts[self.iLow]))
+        for iSev in range(1,4) :
+            self.printTopDict('\n\t' + self.arrSevDescriptions[iSev] + ' Severity Issues: ' , self.sortDict(self.dictLicenseIssues[iSev]),True,5)
 
-for l in json_res['issues']['licenses']:
-    if(l['severity']=='high'):
-        manageDict(dictLicenseHigh, l['title'])
-        countLicenseHigh+=1
-    elif(l['severity']=='medium'):
-        manageDict(dictLicenseMedium, l['title'])
-        countLicenseMedium+=1
-    elif(l['severity']=='low'):
-        manageDict(dictLicenseLow, l['title'])
-        countLicenseLow+=1
+    def printResultDependency(self):
+        print('\nDEPENDENCY ANALYSIS')
+        print('The following lists are identifying what dependencies are problematic by having critical vulns, the most high severity or the most overall count of issues (because even medium/low have value)')
+        for iDepIssueType in range(0,3) :
+            self.printTopDict('\n\tDependency by ' + self.sDepDescriptions[iDepIssueType] + ': ' , self.sortDict(self.dictDepIssues[iDepIssueType]),True,5)
 
-####OUTPUT
+    def printNewsIssues(self):
+        print('\n\n****CVE Watchlist***')
+        print('The following issues are currently critical vulnerabilities that have made the news and were found to be in your codebase')    
+        self.printDictWithDescriptDict('',self.cveWatchlistFound,self.cveWatchlist)
+    
+    def printResults(self):
+            print('Analysis')
+            print('\tID:' + self.id)
+            if(self.reponame != ''):
+                print('\tRepo/Project: ' + self.reponame)
+            print('\n\nThe following analysis lists top 5 items in each area of analysis. Calculation was performed without grouping of vulnerabilities, highlighting every path to each vulnerability')
+            self.printResultVulns()
+            self.printResultLicense()
+            self.printResultDependency()
+            self.printNewsIssues()
+            print('\n\n***END ANALYSIS***')
+   
+    #Analysis worker
+    def analyzeRepo(self,json_res):
+        ###Goal 1 - get counts
+        for v in json_res['issues']['vulnerabilities']:
+            self.manageDict(self.dictDepIssues[self.iDepIssueCounts], v['package'] + ':' + v['version'])
+            if(v['severity']=='high'):
+                self.manageDict(self.dictVulnIssues[self.iHigh], v['title'])
+                self.manageDict(self.dictDepIssues[self.iDepByHighSev], v['package'] + ':' + v['version'])
+                self.vulnIssueCounts[self.iHigh]+=1
+            elif(v['severity']=='medium'):
+                self.manageDict(self.dictVulnIssues[self.iMedium], v['title'])
+                self.vulnIssueCounts[self.iMedium]+=1
+            elif(v['severity']=='low'):
+                self.manageDict(self.dictVulnIssues[self.iLow], v['title'])
+                self.vulnIssueCounts[self.iLow]+=1
 
-print('\n****Target****')
-print('Org ID: ' + org_id)
-print('Project ID: ' + my_js_goof_project_id)
+            if(v['cvssScore'] >= 9.0):
+                self.manageDict(self.dictVulnIssues[self.iCritical], v['title'])
+                self.manageDict(self.dictDepIssues[self.iDepByCriticalSev], v['package'] + ':' + v['version'])
+                self.vulnIssueCounts[self.iCritical]+=1
+        
+            curCVEList = v['identifiers']['CVE']
+            for curCVE in curCVEList:
+                if(self.keyExists(self.cveWatchlist,curCVE)):
+                    self.manageDict(self.cveWatchlistFound, curCVE)
 
-print('\n\n****Overview****')
-print('\n\nThe following analysis lists top 5 items in each area of analysis. Calculation was performed without grouping of vulnerabilities, highlighting every path to each vulnerability')
-print('\n\n****Vulnerability Issues***')
-print('\nCritical Severity: ' + str(countVulnCritical) + ',High Severity: ' + str(countVulnHigh) + ', Medium Severity: ' + str(countVulnMedium) + ', Low Severity: ' + str(countVulnLow) )
-#print('Critical vulns counter:' + str(countVulnCritical))
-printDict('\n\n\tCritical Vulnerabilities: ' , sortDict(dictVulnCritical),True)
-printDict('\n\tHigh Vulnerabilities: ' , sortDict(dictVulnHigh),True)
-printDict('\n\tMedium Vulnerabilities: ' , sortDict(dictVulnMedium),True)
-printDict('\n\tLow Vulnerabilities: ' , sortDict(dictVulnLow),True)
+        for l in json_res['issues']['licenses']:
+            if(l['severity']=='high'):
+                self.manageDict(self.dictLicenseIssues[self.iHigh], l['title'])
+                self.licenseIssueCounts[self.iHigh]+=1
+            elif(l['severity']=='medium'):
+                self.manageDict(self.dictLicenseIssues[self.iMedium], l['title'])
+                self.licenseIssueCounts[self.iMedium]+=1
+            elif(l['severity']=='low'):
+                self.manageDict(self.dictLicenseIssues[self.iLow], l['title'])
+                self.licenseIssueCounts[self.iLow]+=1
 
-print('\n\n****License Issues***')
-print('\n\tHigh Severity: ' + str(countLicenseHigh) + ', Medium Severity: ' + str(countLicenseMedium) + ', Low Severity: ' + str(countLicenseLow))
-printDict('\n\tHigh Severity Licenses Issues: ' , sortDict(dictLicenseHigh),True)
-printDict('\n\tMedium Severity Licenses Issues: ' , sortDict(dictLicenseMedium),True)
-printDict('\n\tLow Severity Licenses Issues: ' , sortDict(dictLicenseLow),True)
+#####ANALYSIS
+##GET NEXT TWO VALUES FROM COMMANDLINE
+org_id = ''
+my_js_goof_project_id = ''
+json_rslts_repo = SnykAPI.snyk_projects_project_issues(org_id, my_js_goof_project_id)
 
-print('\n\n****Dependency Issues***')
-printTopDict('\n\tDependency by Critical Severity Issues: ' , sortDict(dictDepCritical),True,5)
-printTopDict('\n\tDependency by High Severity Issues: ' , sortDict(dictDepHigh),True,5)
-printTopDict('\n\tDependency by Total Issue Count: ' , sortDict(dictDepIssueCount),True,5)
-
-print('\n\n****CVE Watchlist***')
-print('The following issues are currently critical vulnerabilities that have made the news and were found to be in your codebase')    
-printDict('\t', cveWatchlistFound,True)
-
-###We should make the script output to a spreadsheet. From there to generate PDF
-##NOTE THIS WILL GET LIST OF REPOS: https://api.github.com/users/$GHUSER/repos?per_page=100
-##this way we can get list of repos from the github API, then run the CLI like: snyk test https://github.com/ghuser/reponame
-##assuming reponame is given as a parameter
-
-##ADDITIONAL TASK - when calculating severities, you have to match vulnerable module to vuln, maybe use  request:!:REDOS as format?
+masterlog = analysisLog('MASTER LOG', org_id + '/' + my_js_goof_project_id)
+masterlog.analyzeRepo(json_rslts_repo)
+masterlog.printResults()
