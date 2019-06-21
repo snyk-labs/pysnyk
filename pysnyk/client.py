1b9f95840689942a691bf9dacdb86b8c13fe1c3e
Gareth Rushgrove <gareth@morethanseven.net>
2019-06-21 17:38:24 +0100

    Removed the snyk prefix for methods
    The class is already SnykClient, the extra prefix on the methd names and
    variables isn't needed I don't feel goven the scope.

--- pysnyk/client.py
+++ pysnyk/client.py
@@ -7,11 +7,11 @@ from typing import Any, Union, List, Dict
 
 class SnykClient(object):
     def __init__(self, token: str, base_url: str = "https://snyk.io/api/v1/"):
-        self.snyk_api_base_url = base_url
-        self.snyk_api_headers = {"Authorization": "token %s" % token}
+        self.api_base_url = base_url
+        self.api_headers = {"Authorization": "token %s" % token}
 
-        self.snyk_post_api_headers = self.snyk_api_headers
-        self.snyk_post_api_headers["Content-type"] = "application/json"
+        self.post_api_headers = self.api_headers
+        self.post_api_headers["Content-type"] = "application/json"
 
     def _print_json(self, json_obj: Any) -> None:
         print(json.dumps(json_obj, indent=4))
@@ -20,7 +20,7 @@ class SnykClient(object):
         self, api_url: str, obj_json_post_body: Any
     ) -> Any:
         resp = requests.post(
-            api_url, json=obj_json_post_body, headers=self.snyk_post_api_headers
+            api_url, json=obj_json_post_body, headers=self.post_api_headers
         )
         return resp.json()
 
@@ -28,7 +28,7 @@ class SnykClient(object):
         self, api_url: str, obj_json_post_body: Any
     ) -> requests.Response:
         resp = requests.post(
-            api_url, json=obj_json_post_body, headers=self.snyk_post_api_headers
+            api_url, json=obj_json_post_body, headers=self.post_api_headers
         )
         return resp
 
@@ -36,7 +36,7 @@ class SnykClient(object):
         self, api_url: str, obj_json_post_body: Any
     ) -> requests.Response:
         resp = requests.put(
-            api_url, json=obj_json_post_body, headers=self.snyk_post_api_headers
+            api_url, json=obj_json_post_body, headers=self.post_api_headers
         )
         return resp
 
@@ -46,9 +46,9 @@ class SnykClient(object):
 
     # Groups
     # https://snyk.docs.apiary.io/#reference/0/list-members-in-a-group/list-all-members-in-a-group
-    def snyk_groups_members(self, group_id: str) -> Any:
-        full_api_url = "%sorg/%s/members" % (self.snyk_api_base_url, group_id)
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+    def groups_members(self, group_id: str) -> Any:
+        full_api_url = "%sorg/%s/members" % (self.api_base_url, group_id)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         return obj_json_response_content
 
@@ -57,26 +57,26 @@ class SnykClient(object):
     # Organizations -> orgs
     # Lists all Organizations a User belongs to
     # Status: not working - something weird going on where I don't have the permissions to see my orgs
-    def snyk_organizations_orgs(self) -> None:
-        full_api_url = "%sorgs" % (self.snyk_api_base_url)
+    def organizations_orgs(self) -> None:
+        full_api_url = "%sorgs" % (self.api_base_url)
 
         print(full_api_url)
-        # print(snyk_api_headers)
+        # print(api_headers)
         # quit()
 
         print("calling api...")
 
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         self._print_json(obj_json_response_content)
 
     # Organizations -> List Members
     # https://snyk.docs.apiary.io/#reference/organisations/members-in-organisation/list-members
-    def snyk_organizations_list_members(self, org_id: str) -> None:
-        full_api_url = "%sorg/%s/members" % (self.snyk_api_base_url, org_id)
+    def organizations_list_members(self, org_id: str) -> None:
+        full_api_url = "%sorg/%s/members" % (self.api_base_url, org_id)
         print(full_api_url)
 
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         self._print_json(obj_json_response_content)
 
@@ -84,28 +84,24 @@ class SnykClient(object):
 
     # Projects -> List All Projects
     # https://snyk.docs.apiary.io/#reference/projects/all-projects/list-all-projects
-    def snyk_projects_projects(self, org_id: str) -> Any:
-        full_api_url = "%sorg/%s/projects" % (self.snyk_api_base_url, org_id)
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+    def projects_projects(self, org_id: str) -> Any:
+        full_api_url = "%sorg/%s/projects" % (self.api_base_url, org_id)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         return obj_json_response_content
 
     # Projects -> Delete a Project
     # https://snyk.docs.apiary.io/#reference/projects/individual-project/delete-a-project
-    def snyk_projects_delete(self, org_id: str, project_id: str) -> requests.Response:
-        full_api_url = "%sorg/%s/project/%s" % (
-            self.snyk_api_base_url,
-            org_id,
-            project_id,
-        )
-        return requests.delete(full_api_url, headers=self.snyk_api_headers)
+    def projects_delete(self, org_id: str, project_id: str) -> requests.Response:
+        full_api_url = "%sorg/%s/project/%s" % (self.api_base_url, org_id, project_id)
+        return requests.delete(full_api_url, headers=self.api_headers)
 
     # Projects -> List All Issues
     # https://snyk.docs.apiary.io/#reference/projects/project-issues
     # org_id works either like 'demo-applications' or the big hash
-    def snyk_projects_project_issues(self, org_id: str, project_id: str) -> Any:
+    def projects_project_issues(self, org_id: str, project_id: str) -> Any:
         full_api_url = "%sorg/%s/project/%s/issues" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             org_id,
             project_id,
         )
@@ -126,46 +122,46 @@ class SnykClient(object):
         return obj_json_response_content
 
     # https://snyk.docs.apiary.io/#reference/projects/project-ignores/list-all-ignores
-    def snyk_projects_list_all_ignores(self, org_id: str, project_id: str) -> Any:
+    def projects_list_all_ignores(self, org_id: str, project_id: str) -> Any:
         full_api_url = "%sorg/%s/project/%s/ignores" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             org_id,
             project_id,
         )
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         return obj_json_response_content
 
-    def snyk_projects_project_jira_issues_list_all_jira_issues(
+    def projects_project_jira_issues_list_all_jira_issues(
         self, org_id: str, project_id: str
     ) -> Any:
         full_api_url = "%sorg/%s/project/%s/jira-issues" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             org_id,
             project_id,
         )
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         # print_json(obj_json_response_content)
         return obj_json_response_content
 
-    def snyk_projects_get_product_dependency_graph(
+    def projects_get_product_dependency_graph(
         self, org_id: str, project_id: str
     ) -> Any:
         full_api_url = "%sorg/%s/project/%s/dep-graph" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             org_id,
             project_id,
         )
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         return obj_json_response_content
 
-    def snyk_projects_update_project_settings(
+    def projects_update_project_settings(
         self, org_id: str, project_id: str, **kwargs: str
     ) -> requests.Response:
         full_api_url = "%sorg/%s/project/%s/settings" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             org_id,
             project_id,
         )
@@ -186,7 +182,7 @@ class SnykClient(object):
         return self._requests_do_put_api_return_http_response(full_api_url, post_body)
 
     # Integrations
-    def snyk_integrations_import(
+    def integrations_import(
         self,
         org_id: str,
         integration_id: str,
@@ -195,7 +191,7 @@ class SnykClient(object):
         manifest_files: List[str],
     ) -> requests.Response:
         full_api_url = "%sorg/%s/integrations/%s/import" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             org_id,
             integration_id,
         )
@@ -221,13 +217,13 @@ class SnykClient(object):
 
     # Dependencies -> List All Dependencies
     # https://snyk.docs.apiary.io/#reference/dependencies/dependencies-by-organisation
-    def snyk_dependencies_list_all_dependencies_by_project(
+    def dependencies_list_all_dependencies_by_project(
         self, org_id: str, project_id: str, page: int = 1
     ) -> Any:
         results_per_page = 50
         full_api_url = (
             "%sorg/%s/dependencies?sortBy=dependency&order=asc&page=%s&perPage=%s"
-            % (self.snyk_api_base_url, org_id, page, results_per_page)
+            % (self.api_base_url, org_id, page, results_per_page)
         )
         print(full_api_url)
 
@@ -242,7 +238,7 @@ class SnykClient(object):
         results = obj_json_response_content["results"]
 
         if total > (page * results_per_page):
-            next_results = self.snyk_dependencies_list_all_dependencies_by_project(
+            next_results = self.dependencies_list_all_dependencies_by_project(
                 org_id, project_id, page + 1
             )
             results.extend(next_results)
@@ -252,11 +248,11 @@ class SnykClient(object):
     # Licenses
     # List all licenses (in an org)
     # https://snyk.docs.apiary.io/#reference/licenses/licenses-by-organisation
-    def snyk_licenses_list_all_licenses_by_org(
+    def licenses_list_all_licenses_by_org(
         self, org_id: str, project_id: Union[None, str] = None
     ) -> Any:
         full_api_url = "%sorg/%s/licenses?sortBy=license&order=asc" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             org_id,
         )
 
@@ -274,56 +270,56 @@ class SnykClient(object):
 
     # Tests -> test maven
     # https://snyk.docs.apiary.io/#reference/test/maven/test-for-issues-in-a-public-package-by-group-id,-artifact-id-and-version
-    def snyk_test_maven(
+    def test_maven(
         self, package_group_id: str, package_artifact_id: str, version: str, org_id: str
     ) -> Any:
         full_api_url = "%stest/maven/%s/%s/%s?org=%s" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             package_group_id,
             package_artifact_id,
             version,
             org_id,
         )
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         return obj_json_response_content
 
     # https://snyk.docs.apiary.io/#reference/test/rubygems/test-for-issues-in-a-public-gem-by-name-and-version
-    def snyk_test_rubygem(self, gem_name: str, gem_version: str, org_id: str) -> Any:
+    def test_rubygem(self, gem_name: str, gem_version: str, org_id: str) -> Any:
         full_api_url = "%stest/rubygems/%s/%s?org=%s" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             gem_name,
             gem_version,
             org_id,
         )
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         return obj_json_response_content
 
     # https://snyk.docs.apiary.io/#reference/test/pip/test-for-issues-in-a-public-package-by-name-and-version
-    def snyk_test_python_package(
+    def test_python_package(
         self, package_name: str, package_version: str, org_id: str
     ) -> Any:
         full_api_url = "%stest/pip/%s/%s?org=%s" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             package_name,
             package_version,
             org_id,
         )
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         return obj_json_response_content
 
     # https://snyk.docs.apiary.io/#reference/test/npm/test-for-issues-in-a-public-package-by-name-and-version
-    def snyk_test_npm_package(
+    def test_npm_package(
         self, package_name: str, package_version: str, org_id: str
     ) -> Any:
         full_api_url = "%stest/npm/%s/%s?org=%s" % (
-            self.snyk_api_base_url,
+            self.api_base_url,
             package_name,
             package_version,
             org_id,
         )
-        resp = requests.get(full_api_url, headers=self.snyk_api_headers)
+        resp = requests.get(full_api_url, headers=self.api_headers)
         obj_json_response_content = resp.json()
         return obj_json_response_content
