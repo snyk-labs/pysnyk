a21988b19bbe9dea15769cee96ed8a57e06c644c
Gareth Rushgrove <gareth@morethanseven.net>
2019-06-22 16:12:50 +0100

    Added a few more unit tests for the client
    Lots more to do, mainly with mocking the HTTP requests. But starting
    with a few of the initiasation tests.

--- snyk/test_client.py
+++ snyk/test_client.py
@@ -15,3 +15,12 @@ class TestSnykClient(object):
         url = "https://notsnyk.io/api/v1/"
         client = SnykClient("token", url)
         assert client.api_url == url
+
+    def test_token_added_to_headers(self, client):
+        assert client.api_headers["Authorization"] == "token token"
+
+    def test_token_added_to_post_headers(self, client):
+        assert client.api_post_headers["Authorization"] == "token token"
+
+    def test_post_headers_use_correct_mimetype(self, client):
+        assert client.api_post_headers["Content-Type"] == "application/json"
