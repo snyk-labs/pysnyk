dcad9e6f65d3ddb50231446b87f83fb2649d7bcb
Gareth Rushgrove <gareth@morethanseven.net>
2019-06-22 11:08:53 +0100

    Moved lots of other methods to the relevant objects
    I've not converted all of these over to using objects in the response
    yet, but you can call them on the relevant objects now.

--- snyk/models.py
+++ snyk/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Optional, List, Any
+from typing import Optional, List, Dict, Any
 
 import requests
 from mashumaro import DataClassJSONMixin  # type: ignore
@@ -30,6 +30,15 @@ class Organization(DataClassJSONMixin):
         for member_data in resp.json():
             members.append(Member.from_dict(member_data))
         return members
+
+    # TODO: convert to objects
+    # https://snyk.docs.apiary.io/#reference/licenses/licenses-by-organisation
+    def licenses(self, client) -> requests.Response:
+        path = "org/%s/licenses?sortBy=license&order=asc" % self.id
+
+        post_body: Dict[str, Dict[str, List[str]]] = {"filters": {}}
+
+        return self._requests_do_post_api_return_http_response(path, post_body)
 
 
 @dataclass
@@ -83,6 +92,83 @@ class Project(DataClassJSONMixin):
 
         resp = client._requests_do_post_api_return_http_response(path, post_body)
         return IssueSet.from_dict(resp.json())
+
+    # TODO: convert to object
+    # https://snyk.docs.apiary.io/#reference/projects/project-ignores/list-all-ignores
+    def ignores(self, client) -> Any:
+        path = "org/%s/project/%s/ignores" % (self.organization.id, self.id)
+        resp = client._requests_do_get_return_http_response(path)
+        return resp.json()
+
+    # TODO: convert to objects
+    def jira_issues(self, client) -> Any:
+        path = "org/%s/project/%s/jira-issues" % (self.organization.id, self.id)
+        resp = client._requests_do_get_return_http_response(path)
+        return resp.json()
+
+    # TODO: convert to objects
+    def dependency_graph(self, client) -> Any:
+        path = "org/%s/project/%s/dep-graph" % (self.organization.id, self.id)
+        resp = client._requests_do_get_return_http_response(path)
+        return resp.json()
+
+    # TODO: move pagingation per page value to constant
+    # TODO: convert to objects
+    # https://snyk.docs.apiary.io/#reference/dependencies/dependencies-by-organisation
+    def dependencies(self, client, page: int = 1) -> Any:
+        results_per_page = 50
+        path = "org/%s/dependencies?sortBy=dependency&order=asc&page=%s&perPage=%s" % (
+            self.organization.id,
+            page,
+            results_per_page,
+    ***REMOVED***
+
+        post_body = {"filters": {"projects": [self.id]}}
+
+        resp = self._requests_do_post_api_return_http_response(path, post_body)
+        obj_json_response_content = resp.json()
+
+        total = obj_json_response_content[
+            "total"
+        ]  # contains the total number of results (for pagination use)
+        results = obj_json_response_content["results"]
+
+        if total > (page * results_per_page):
+            next_results = self.dependencies(client, page + 1)
+            results.extend(next_results)
+            return results
+        return results
+
+    # TODO: convert to objects
+    # https://snyk.docs.apiary.io/#reference/licenses/licenses-by-organisation
+    def licenses(self, client) -> requests.Response:
+        path = "org/%s/licenses?sortBy=license&order=asc" % self.organization.id
+
+        post_body: Dict[str, Dict[str, List[str]]] = {
+            "filters": {"projects": [self.id]}
+        }
+
+        return self._requests_do_post_api_return_http_response(path, post_body)
+
+    def projects_update_project_settings(
+        self, client, **kwargs: str
+***REMOVED*** -> requests.Response:
+        path = "org/%s/project/%s/settings" % (self.organization.id, self.id)
+
+        post_body = {}
+
+        if "pullRequestTestEnabled" in kwargs:
+            post_body["pullRequestTestEnabled"] = kwargs["pullRequestTestEnabled"]
+
+        if "pullRequestFailOnAnyVulns" in kwargs:
+            post_body["pullRequestFailOnAnyVulns"] = kwargs["pullRequestFailOnAnyVulns"]
+
+        if "pullRequestFailOnlyForHighSeverity" in kwargs:
+            post_body["pullRequestFailOnlyForHighSeverity"] = kwargs[
+                "pullRequestFailOnlyForHighSeverity"
+            ]
+
+        return client._requests_do_put_api_return_http_response(path, post_body)
 
 
 @dataclass
