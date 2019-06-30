1bf67c67a90b09c2b9352577c4b857e6d4d92ce5
Gareth Rushgrove <gareth@morethanseven.net>
2019-06-30 18:28:07 +0100

    Snyk keys start with a lower case letter

--- snyk/utils.py
+++ snyk/utils.py
@@ -2,4 +2,10 @@ import re
 
 
 def snake_to_camel(word):
-    return "".join(x.capitalize() or "_" for x in word.split("_"))
+    return lower_case_first_letter(
+        "".join(x.capitalize() or "_" for x in word.split("_"))
+    )
+
+
+def lower_case_first_letter(word):
+    return word[:1].lower() + word[1:] if word else ""
