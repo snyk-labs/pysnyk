d1915524318c85aeb56f7c57446e2b4efa268783
kriti-d <kriti.dogra@snyk.io>
2020-03-06 13:31:52 -0500

    Update example to print sev

--- examples/api-demo-4-list-licenses-by-org.py
+++ examples/api-demo-4-list-licenses-by-org.py
@@ -26,6 +26,7 @@ licenses = client.organizations.get(org_id).licenses.all()
 print("\n\nNumber of licenses: %s" % len(licenses))
 for license in licenses:
     print("\nLicense: %s" % (license.id))
+    print("Severity: %s" % (license.severity))
 
     if show_dependencies:
         print("  Dependencies:")
