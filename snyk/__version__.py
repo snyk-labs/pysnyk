d8c59fcc8c6cc8ea9f4116c795367a925d8cb90b
Nir Fuchs <nir.fuchs@snyk.io>
2020-06-09 08:48:57 +0300

    Fix user agent string

--- snyk/__version__.py
+++ snyk/__version__.py
@@ -1,7 +1,11 @@
+from importlib_metadata import version	# type: ignore
 import poetry_version  # type: ignore
 
 __title__ = "snyk"
 __description__ = "An API client for the Snyk API."
 __url__ = "https://snyk.docs.apiary.io"
 __license__ = "MIT"
-__version__ = poetry_version.extract(source_file=__file__)
+try:
+    __version__ = version('pysnyk')
+except:
+    __version__ = poetry_version.extract(source_file=__file__)
