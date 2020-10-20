2b984324e9a3a582c8881c254fe25bb09a5e089e
Gareth Rushgrove <gareth@morethanseven.net>
2020-10-20 08:09:33 +0100

    Added the Kubernetes cluster information to the project model (#90)
    * Added the Kubernetes cluster information to the project model
    
    * The docs are incorrect, this is type not packageManager

--- snyk/models.py
+++ snyk/models.py
@@ -430,7 +430,7 @@ class Dependency(DataClassJSONMixin):
     latestVersionPublishedDate: Optional[str] = None
     firstPublishedDate: Optional[str] = None
     isDeprecated: Optional[bool] = None
-    packageManager: Optional[str] = None
+    type: Optional[str] = None
     deprecatedVersions: Optional[List[Any]] = field(default_factory=list)
     dependenciesWithIssues: Optional[List[Any]] = field(default_factory=list)
 
@@ -454,6 +454,7 @@ class Project(DataClassJSONMixin):
     imageId: Optional[str] = None
     imageBaseImage: Optional[str] = None
     imagePlatform: Optional[str] = None
+    imageCluster: Optional[str] = None
     hostname: Optional[str] = None
     remoteRepoUrl: Optional[str] = None
     branch: Optional[str] = None
